7bf53c0e0286fd48a496dd52ad3b083eb56b880d
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-02-10 21:39:46 -0300

    Algoritmo que armazene números em dois vetores inteiros de cinco elementos cada. Gere e imprima o vetor soma.

---
+++ CódigosF.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main()
+		{		 
+			int[] vetorA = new int[5];
+			int[] vetorB = new int[5];
+			int[] vetorSoma = new int[5];
+			
+			Console.WriteLine("Insira 5 valores para o vetorA: ");
+			for (int i = 0; i < vetorA.Length; i++)
+			{
+				Console.Write("Valor {0}: ", i + 1);
+				vetorA[i] = int.Parse(Console.ReadLine());
+			}
+			Console.WriteLine("\n\rDigite 5 valores para o vetorB: ");
+			for (int i = 0; i < vetorB.Length; i++)
+			{
+				Console.Write("Valor {0}: ", i + 1);
+				vetorB[i] = int.Parse(Console.ReadLine());
+			}			
+			for (int i = 0; i < vetorSoma.Length; i++)
+			{
+				vetorSoma[i]
+					= vetorA[i] + vetorB[i];
+			}
+			Console.Clear();
+			Console.WriteLine("\nVetor A: " + string.Join(", ", vetorA));
+       		Console.WriteLine("Vetor B: " + string.Join(", ", vetorB));
+       		Console.WriteLine("Vetor Soma: " + string.Join(", ", vetorSoma));
+			Console.WriteLine();
+			Console.ReadKey(true);
+		}
+    
+	}
+  
+}
