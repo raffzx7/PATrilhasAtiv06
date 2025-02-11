4b410f948dafe044dc951f9801e5b9554fc39a81
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-02-10 21:01:54 -0300

    Create CódigoH.cs

---
+++ CódigoH.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace PATrilhasAtiv06
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("insira um número inteiro positivo: ");
+			var numero = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine("\n\rEsses são seus divisores: ");
+			
+			for (int contador = 1; contador <= numero; contador++){
+				
+				if (numero % contador == 0) {
+	
+				Console.Write(+contador+ " ");
+			}
+		}
+			Console.WriteLine();
+			Console.ReadKey(true);
+		}
+    
+	}
+  
+}
